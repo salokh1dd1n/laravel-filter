1cb3d5055240573b229cc96f7cb0cd4ea88eeb49
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-26 21:24:31 +0500

    Updated User Filter

--- app/Filters/UserFilter.php
+++ app/Filters/UserFilter.php
@@ -8,44 +8,36 @@ use Illuminate\Http\Request;
 class UserFilter extends CoreFilter
 {
 
-    /**
-     * @param UserPayload $payload
-     */
-    public function __construct(Request $request)
-    {
-        parent::__construct($request);
-    }
-
     public function name($value)
     {
-        $this->model->where('name', 'like', "%$value%");
+        $this->builder->where('name', 'like', "%$value%");
     }
 
     public function email($value)
     {
-        $this->model->where('email', 'like', "%$value%");
+        $this->builder->where('email', 'like', "%$value%");
     }
 //
     public function is_active($value)
     {
         if ($value == '1') {
-            $this->model->where('is_active', 1);
+            $this->builder->where('is_active', 1);
         } else {
-            $this->model->where('is_active', 0);
+            $this->builder->where('is_active', 0);
         }
     }
-//
+
     public function birthday($value)
     {
         if (!$value) return;
-        $this->model->where('birthday', '>', $value);
+        $this->builder->where('birthday', '>', $value);
     }
-//
+
     public function gender($value)
     {
         if (!$value) return;
 
-        $this->model->where('gender', $value);
+        $this->builder->where('gender', $value);
     }
 
 }
