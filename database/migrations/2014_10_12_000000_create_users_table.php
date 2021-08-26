302db7caf474e87695f3eadc069ea9c4c109eeaa
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-26 17:28:45 +0500

    Updated user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('is_active')->default(0);
+            $table->timestamp('birthday');
+            $table->enum('gender', ['male', 'female']);
             $table->timestamps();
         });
     }
