b1cb4aefa2a85a0144ee6512eda25be34f993a46
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-26 21:23:43 +0500

    Updated Core Filter

--- app/Filters/CoreFilter.php
+++ app/Filters/CoreFilter.php
@@ -4,30 +4,16 @@ namespace App\Filters;
 
 class CoreFilter
 {
-    protected $model;
+    protected object $builder;
 
-    protected $payload;
-
-    /**
-     * @param $payload
-     */
-    public function __construct($payload)
+    public function apply($builder, $attributes)
     {
-        $this->payload = $payload;
-    }
-
-    public function apply($model)
-    {
-        $this->model = $model;
-        foreach ($this->filters() as $filter => $value) {
+        $this->builder = $builder;
+        foreach ($attributes as $filter => $value) {
             if (method_exists($this, $filter)) {
                 $this->$filter($value);
             }
         }
     }
 
-    public function filters(): array
-    {
-        return $this->payload->all();
-    }
 }
