d8cbe8003d7ddd99810a54cd37e50b2cc4001b2a
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-26 21:24:24 +0500

    Updated User controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers;
 use App\Filters\UserFilter;
 use App\Models\User;
 use App\Payloads\UserPayload;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(Request $request, UserFilter $filters)
-    {
-        $payload = UserPayload::getFilters($request->all());
 
-        $users = User::filter($filters)->get();
+    /**
+     * @param Request $request
+     * @param UserFilter $filter
+     * @return Application|Factory|View
+     */
+    public function index(Request $request, UserFilter $filter): View|Factory|Application
+    {
+        $attributes = UserPayload::getFilters($request->all());
+        $users = User::filter($filter, $attributes)->get();
 
         return view('users', compact('users'));
     }
